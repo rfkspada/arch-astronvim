8f4cfc9c22e2e10f94960a6b3523dbb62accf163
rfkspada <rfkspada@gmail.com>
2026-08-07 13:44:52 -0300

    Corrigindo o comportamento do tab para expandit o snippet antes de pular entre placeholdaers

--- lua/plugins/blink-cmp.lua
+++ lua/plugins/blink-cmp.lua
@@ -3,13 +3,25 @@ return {
   "saghen/blink.cmp",
   opts = {
     keymap = {
-      preset = "enter", -- Use 'enter' as a base or 'none'
-      ["<Tab>"] = { "select_next", "snippet_forward", "fallback" },
+      preset = "default",
+      ["<Tab>"] = {
+        "select_next",
+        -- Função para só expandir o snippet, sem pular entre placeholders!
+        function()
+          local luasnip = require "luasnip"
+          if luasnip.expandable() then
+            vim.schedule(function() luasnip.expand() end)
+            return true -- Avisa ao blink que o Tab foi lidado e impede o fallback
+          end
+        end,
+        "snippet_forward",
+        "fallback",
+      },
       ["<S-Tab>"] = { "select_prev", "snippet_backward", "fallback" },
     },
     -- Optional: auto-insert the selection while tabbing
     completion = {
-      list = { selection = { preselect = false, auto_insert = true } },
+      list = { selection = { preselect = true, auto_insert = false } },
       menu = { auto_show = false },
     },
     sources = {
