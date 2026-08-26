ca776f2d7bf6060299dd710f314dc4df4c8af4e0
rfkspada <rfkspada@gmail.com>
2026-08-26 14:39:12 -0300

    adding render markdown

---
+++ lua/plugins/render-markdown.lua
@@ -0,0 +1,31 @@
+return {
+  "MeanderingProgrammer/render-markdown.nvim",
+  cmd = "RenderMarkdown",
+  ft = function()
+    local plugin = require("lazy.core.config").spec.plugins["render-markdown.nvim"]
+    local opts = require("lazy.core.plugin").values(plugin, "opts", false)
+    return opts.file_types or { "markdown" }
+  end,
+  dependencies = {
+    "nvim-treesitter/nvim-treesitter",
+  },
+  specs = {
+    {
+      "AstroNvim/astrocore",
+      optional = true,
+      ---@type AstroCoreOpts
+      opts = {
+        treesitter = { ensure_installed = { "html", "markdown", "markdown_inline" } },
+      },
+    },
+  },
+  opts = {
+    latex = {
+      enabled = true,
+      converter = "latex2text",
+      highlight = "RenderMarkdownMath",
+      top_pad = 0,
+      bottom_pad = 0,
+    },
+  },
+}
