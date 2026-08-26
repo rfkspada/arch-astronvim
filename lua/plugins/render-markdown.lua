d079571b6ea3984f44b56580a76c62a3a226614f
rfkspada <rfkspada@gmail.com>
2026-08-26 15:50:21 -0300

    configuring render-markdown plugin

--- lua/plugins/render-markdown.lua
+++ lua/plugins/render-markdown.lua
@@ -20,12 +20,8 @@ return {
     },
   },
   opts = {
-    latex = {
-      enabled = true,
-      converter = "latex2text",
-      highlight = "RenderMarkdownMath",
-      top_pad = 0,
-      bottom_pad = 0,
+    anti_conceal = {
+      enabled = false,
     },
   },
 }
