8791209e9eecf9737843454aa6ffeca58cde0aee
rfkspada <rfkspada@gmail.com>
2026-09-22 11:59:18 -0300

    Adding congs for vimtex in community.lua

--- lua/community.lua
+++ lua/community.lua
@@ -10,4 +10,36 @@ return {
   { import = "astrocommunity.pack.lua" },
   { import = "astrocommunity.markdown-and-latex.vimtex" },
   -- import/override with your plugins folder
+
+  -- Sobrescreve o vimtex importado acima para fixar a tag desejada
+  {
+    "lervag/vimtex",
+    tag = "v2.18", -- ⚠️ Mude aqui para a versão que você precisa (ex: v2.14, v2.15, etc.)
+    init = function()
+      -- Lista original do VimTeX, removendo APENAS o "bbl"
+      vim.g.vimtex_compiler_clean_outputs = {
+        "aux",
+        "blg",
+        "glg",
+        "glo",
+        "gls",
+        "idx",
+        "ilg",
+        "ind",
+        "ist",
+        "lof",
+        "log",
+        "lot",
+        "nav",
+        "out",
+        "snm",
+        "toc",
+        "vrb",
+        "fls",
+        "fdb_latexmk",
+        "synctex.gz",
+        "pdfsync",
+      }
+    end,
+  },
 }
